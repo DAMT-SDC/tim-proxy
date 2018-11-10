0ab1f636e199644f21c6ce9e9bd75b5d2bb1e8ad
tim <timothy.tran835@gmail.com>
2018-11-10 14:34:35 -0800

    committed file for proxy server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const cors = require('cors');
-const { env } = require('./.env');
+// const { env } = require('./.env');
 
 const app = express();
 
